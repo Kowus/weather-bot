d91f9d3b90ca3412552746962f2b541b67bfb654
Kowus <barnabas@elite-education.org>
2017-03-17 18:42:53 +0000

    added line event

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 'use strict';
 
-const Readline = require('readline');
-const rl = Readline.createInterface({
+const readline = require('readline');
+const rl = readline.createInterface({
 	input: process.stdin,
 	output: process.stdout,
 	terminal: false
 });
 
-rl.setPrompt('foster >');
+rl.setPrompt('foster > ');
+rl.prompt();
+rl.on('line', reply => {
+	console.log(`You said ${reply}`);
+});
