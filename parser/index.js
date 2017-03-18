643096cc6fa7d35987b9932220bc943275a2dda6
Kowus <barnabas@elite-education.org>
2017-03-18 13:19:10 +0000

    getting prefixes from dictionary

--- parser/index.js
+++ parser/index.js
@@ -1,12 +1,23 @@
 'use strict';
 const colors = require('colors');
+const dictionary = require('./dictionary');
+
 let getFeel = temp => {
-    if (temp < 5) { return "shivering cold"; }
-    else if (temp < 15) return 'pretty cold'
-    else if (temp < 25) return 'moderately cold'
-    else if (temp < 32) return 'quite warm'
-    else if (temp < 40) return 'very hot'
-    else return 'super hot'
+    if (temp < 5) return "shivering cold";
+    else if (temp < 15) return 'pretty cold';
+    else if (temp < 25) return 'moderately cold';
+    else if (temp < 32) return 'quite warm';
+    else if (temp < 40) return 'very hot';
+    else return 'super hot';
+}
+
+let getPrefix = (conditionCode, tense = 'present') => {
+    let findPrefix = dictionary[tense].find(item => {
+        if (item.codes.indexOf(Number(conditionCode)) > -1) {
+            return true;
+        }
+    });
+    return findPrefix || "";
 }
 
 let currentWeather = response => {
